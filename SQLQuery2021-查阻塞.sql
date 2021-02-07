7aa88567cdfbaa8fb94ed97003cc0e031b6c979d
damon <damon@DESKTOP-5BEL4F0>
2021-02-07 11:55:03 +0800

    修改了查阻塞脚本

--- SQLQuery2021-查阻塞.sql
+++ SQLQuery2021-查阻塞.sql
@@ -12,7 +12,7 @@ WITH CTE_SID ( bsid, sid, sql_handle )
                FROM     sys.dm_exec_requests A
                         JOIN CTE_SID B ON A.session_id = B.bsid
              )
-    SELECT  C.bsid as blk,
+    SELECT  C.bsid AS blk,
             C.sid ,
             S.login_name ,
             S.host_name ,
@@ -43,49 +43,41 @@ WITH CTE_SID ( bsid, sid, sql_handle )
                FROM     sys.dm_exec_requests A
                         JOIN CTE_SID B ON A.session_id = B.bsid
              )
-    SELECT  C.bsid as 阻塞者,
-            C.sid as 进程号,
-            S.login_name as 登录名,
-            S.host_name as 主机名,
-            S.status as 状态,
-            S.cpu_time as 处理器时间,
-            S.memory_usage as 内存使用量,
-            S.last_request_start_time as 最后请求开始时间,
-            S.last_request_end_time as 最后请求结束时间,
-            S.logical_reads as 逻辑读,
-            S.row_count as 行计数,
-            Q.text as 执行的文本
+    SELECT  C.bsid AS 阻塞者,
+            C.sid AS 进程号,
+            S.login_name AS 登录名,
+            S.host_name AS 主机名,
+            S.status AS 状态,
+            S.cpu_time AS 处理器时间,
+            S.memory_usage AS 内存使用量,
+            S.last_request_start_time AS 最后请求开始时间,
+            S.last_request_end_time AS 最后请求结束时间,
+            S.logical_reads AS 逻辑读,
+            S.row_count AS 行计数,
+            Q.text AS 执行的文本
    FROM    CTE_SID C
             JOIN sys.dm_exec_sessions S ON C.sid = S.session_id
             CROSS APPLY sys.dm_exec_sql_text(C.sql_handle) Q
     ORDER BY C.sid
 
 
+SELECT      C.blocking_session_id AS 阻塞者,
+            C.session_id AS 进程号,
+            S.login_name AS 登录名,
+            S.host_name AS 主机名,
+            S.status AS 状态,
+            S.cpu_time AS 处理器时间,
+            S.memory_usage AS 内存使用量,
+            S.last_request_start_time AS 最后请求开始时间,
+            S.last_request_end_time AS 最后请求结束时间,
+            S.logical_reads AS 逻辑读,
+            S.row_count AS 行计数,
+            Q.text AS 执行的文本
+FROM        sys.dm_exec_requests C
+JOIN        sys.dm_exec_sessions S ON C.session_id = S.session_id
+CROSS APPLY sys.dm_exec_sql_text(C.sql_handle) Q
+WHERE       C.blocking_session_id <> 0
 
-SELECT  blocking_session_id ,
-        session_id ,
-        sql_handle
-FROM    sys.dm_exec_requests 
-WHERE   blocking_session_id <> 0
-UNION ALL
-SELECT  A.blocking_session_id ,
-        A.session_id ,
-        A.sql_handle
-FROM    sys.dm_exec_requests A
-JOIN    sys.dm_exec_requests B 
-    ON  A.blocking_session_id = B.session_id
+dbcc inputbuffer(4975)
 
-
-select      C.blocking_session_id as 阻塞者,
-            C.session_id as 进程号,
-            S.login_name as 登录名,
-            S.host_name as 主机名,
-            S.status as 状态,
-            S.cpu_time as 处理器时间,
-            S.memory_usage as 内存使用量,
-            S.last_request_start_time as 最后请求开始时间,
-            S.last_request_end_time as 最后请求结束时间,
-            S.logical_reads as 逻辑读,
-            S.row_count as 行计数,
-            Q.text as 执行的文本
-from        sys.dm_exec_requests A
+  begin tran    select * from #empl (holdlock)    sleep     
